6d831fa916d0e38b22e41ce55f4e43cb85bd2785
HarleyB123-LCWC <144430651+HarleyB123-LCWC@users.noreply.github.com>
2023-12-14 12:45:24 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ function CheckData() {
 
     var firstemail = document.getElementById("firstemail").value    
     var secondemail = document.getElementById("confirmemail").value
+    var nameinput = document.getElementById("nameinput").value
+    var dateinput = document.getElementById("dateinput").value
 
-    if (firstemail === secondemail) {
-        alert("Emails are Valid")
-    } else {
-        alert("Emails are not valid")
+    console.log(dateinput);
+    if (firstemail === "" || secondemail === "") {
+	alert("Email Entry Must have a input")
+    } else if (nameinput === "") {
+	alert("Name Must have an Input")
+    } else if (dateinput === "") {
+	alert("Date of Birth Must have an input")
     }
+    else {
+    	if (firstemail === secondemail) {
+    	    alert("Emails are Valid")
+    	} else {
+    	    alert("Emails are not valid")
+    	}
+    }
+
+
 
 }
